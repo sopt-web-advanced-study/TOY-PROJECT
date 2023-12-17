361d727c939c85f04ed6a1d1398499d5fa6377ce
imeureka <lky062@naver.com>
2023-12-18 00:50:28 +0900

    [ init ] Nextjs 초기세팅

---
+++ toy-project/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { RecoilRoot } from 'recoil';
+
+export default function Home() {
+  return (
+    <>
+     <RecoilRoot> 
+     <h1 className="text-red">
+     심심스 토이프로젝트 1조     </h1>     
+      
+     </RecoilRoot>
+      
+    </>
+  );
+}
