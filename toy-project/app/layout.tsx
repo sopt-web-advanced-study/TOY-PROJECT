c06e1f04b13cd623b9c2984bd7e39c691f6718d3
HAE2NI <hae0_02ni@ewhain.net>
2023-12-19 00:19:39 +0900

    [ design ] common style 공통으로 뺴기

--- toy-project/app/layout.tsx
+++ toy-project/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import '@/styles/common.css';
 
 export const metadata: Metadata = {
   title: '심심스 1조',
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body className="commonFlex">{children}</body>
     </html>
   );
 }
