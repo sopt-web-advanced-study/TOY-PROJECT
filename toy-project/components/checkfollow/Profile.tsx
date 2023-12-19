48f9a9d3fe3e66565e47d3975ecc6c4c7beee196
Doyun Lee <doyn511@ajou.ac.kr>
2023-12-20 02:16:41 +0900

    [ design ] Profile component UI

---
+++ toy-project/components/checkfollow/Profile.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import Image from 'next/image';
+
+export default function Profiole() {
+  return (
+    <section className="p-y-[30px] p-x-[25px] commonBackground flex h-[150px] h-[150px] w-[340px] gap-[12px]">
+      <div className="w-2/5 pl-[40px] pt-[28px]">
+        <div className="absolute h-[90px] w-[90px] rounded-full bg-grey01">
+          <Image
+            src="/santahat.png"
+            alt="산타모자"
+            width={40}
+            height={40}
+            className="absolute left-[-5px] top-[-20px] rotate-[340deg]"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col justify-center gap-[12px]">
+        <span className="text-title01">NAME</span>
+        <span className="text-subTitle01">BIO</span>
+        <div className="flex gap-[22px] ">
+          <button className="commonButton h-[25px] w-[58px] bg-red">팔로워</button>
+          <button className="commonButton h-[25px] w-[58px] bg-green">팔로잉</button>
+        </div>
+      </div>
+    </section>
+  );
+}
