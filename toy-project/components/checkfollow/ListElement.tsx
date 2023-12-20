19d2c387acdad73cf347914b739d8a7fbea824f5
Doyun Lee <doyn511@ajou.ac.kr>
2023-12-20 17:11:30 +0900

    [ design ] check image 추가

--- toy-project/components/checkfollow/ListElement.tsx
+++ toy-project/components/checkfollow/ListElement.tsx
@@ -10,7 +10,8 @@ export default function ListElement(props: userInfoTypes) {
   const { id, userName, imageUrl } = props;
 
   return (
-    <li className="flex h-[40px] w-full items-center gap-[27px] px-[44px]">
+    <li className="flex h-[40px] w-full items-center gap-[10px] px-[44px]">
+      <i className="text-subTitle01">✔️</i>
       {/* <Image src={imageUrl} alt="팔로우리스트-프로필이미지" width={40} height={40} className="rounded-full" /> */}
       <div className="h-[40px] w-[40px] rounded-full bg-grey01"></div>
       <span className="text-title01">{userName}</span>
