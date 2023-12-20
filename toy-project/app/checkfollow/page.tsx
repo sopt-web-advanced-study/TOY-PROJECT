889f78bf40bc0f67980c5ad99152c3a742b8f9cd
Doyun Lee <doyn511@ajou.ac.kr>
2023-12-20 16:47:13 +0900

    [ design ] checkfollow page 전체 UI

---
+++ toy-project/app/checkfollow/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import FollowList from '@/components/checkfollow/FollowList';
+import FollowOrNotBtn from '../../components/checkfollow/FollowOrNotBtn';
+import Profile from '../../components/checkfollow/Profile';
+
+export default function CheckFollow() {
+  return (
+    <article className="flex flex-col px-[25px] py-[37px]">
+      <Profile />
+      <FollowOrNotBtn />
+      <FollowList />
+      <button className="mt-[34px] h-[45px] rounded-[5px] border-2 border-solid border-grey02 bg-sub01 text-title01 text-white ">
+        맞팔하기
+      </button>
+    </article>
+  );
+}
