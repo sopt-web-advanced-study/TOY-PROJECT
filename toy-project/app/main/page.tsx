b2fd4aae43c7ca5f4d123a4fa8814006ed631278
HAE2NI <hae0_02ni@ewhain.net>
2023-12-21 15:44:37 +0900

    [ design ] main page

---
+++ toy-project/app/main/page.tsx
@@ -0,0 +1,10 @@
+import MainContainer from '@/components/main/MainContainer';
+
+export default function main() {
+  return (
+    <section className="commonFlex h-[84.4rem] w-[29.6rem] flex-col ">
+      {/* <img src={santahat} alt="산타모자" className="h-[18.6rem] w-[14rem]" /> */}
+      <MainContainer />
+    </section>
+  );
+}
