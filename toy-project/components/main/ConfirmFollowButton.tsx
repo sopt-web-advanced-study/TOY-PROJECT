0d5f29e52fba1cf667a1e4f182f98e2023bc477c
HAE2NI <hae0_02ni@ewhain.net>
2023-12-21 15:57:36 +0900

    [ design ] hover 이벤트 추가

--- toy-project/components/main/ConfirmFollowButton.tsx
+++ toy-project/components/main/ConfirmFollowButton.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function ConfirmFollowButton() {
-  return <div className="commonFlex followBackButton">나의 맞팔 확인하러 가기</div>;
+  return <div className="commonFlex followBackButton  hover:bg-grey02">나의 맞팔 확인하러 가기</div>;
 }
--- toy-project/components/main/TokenMakeButton.tsx
+++ toy-project/components/main/TokenMakeButton.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-
 export default function TokenMakeButton() {
   return (
     <div className="flex w-[29.6rem]">
-      <button className="commonButton h-[2.5rem] w-[14.1rem] bg-sub02">Github Token 만들러 가기</button>
+      <button className="commonButton h-[2.5rem] w-[14.1rem] bg-sub02  hover:bg-grey02">
+        Github Token 만들러 가기
+      </button>
     </div>
   );
 }
