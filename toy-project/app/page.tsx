8b7467ea7261521d06855c8b36d95112314d748f
HAE2NI <hae0_02ni@ewhain.net>
2023-12-21 15:57:03 +0900

    [ etc ] main라우터를 기본 page.tsx로 옮김

--- toy-project/app/page.tsx
+++ toy-project/app/page.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import { RecoilRoot } from 'recoil';
+import MainContainer from '@/components/main/MainContainer';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <RecoilRoot>
-        <section className="commonBackground commonFlex h-[480px] w-[340px]">
-          <button className="commonFlex followBackButton ">나의 맞팔 확인, 맞팔하기 버튼 </button>
-          <button className="commonFlex commonButton h-[30px] w-[30px] bg-red">팔로잉 </button>
-          <h1 className="text-white">gd</h1>
+        <section className="commonFlex h-[84.4rem] w-[29.6rem] flex-col ">
+          <Image
+            className="commonFlex absolute left-[-1rem] top-[10rem] z-10"
+            src="/santahat.png"
+            alt="산타모자"
+            height={186}
+            width={140}
+          />
+          <MainContainer />
         </section>
       </RecoilRoot>
     </>
