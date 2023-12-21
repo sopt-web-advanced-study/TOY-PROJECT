260d68bd3f3c6c8b1267b9601be76d7605f3cbea
HAE2NI <hae0_02ni@ewhain.net>
2023-12-21 15:44:51 +0900

    [ design ] github token 입력 input

---
+++ toy-project/components/main/TokenInput.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function TokenInput() {
+  return (
+    <input
+      className="h-[4.5rem] w-[29.6rem] rounded-md border-2 border-grey01  pl-[0.5rem] text-subTitle01"
+      placeholder="Github Token을 입력해주세요"
+      type="text"
+    />
+  );
+}
