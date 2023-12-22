00718efde6480108f51daa836a9aa254c30b599d
HAE2NI <hae0_02ni@ewhain.net>
2023-12-22 15:50:16 +0900

    [ fix ] 에러 해결 코드 제안

--- toy-project/apis/getProfileInfo.ts
+++ toy-project/apis/getProfileInfo.ts
@@ -1,18 +1,51 @@
-import { ProfileType } from '@/types/types';
+// import { ProfileType } from '@/types/types';
 import axios from 'axios';
-import { SetterOrUpdater } from 'recoil';
+import { selector } from 'recoil';
 
-export const getProfileInfo = async (setUserInfo: SetterOrUpdater<ProfileType>) => {
-  try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user`, {
-      headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_GIT_TOKEN}`,
-      },
-    });
-    const { login, avatar_url, bio, following, followers } = response.data;
-    setUserInfo({ login, avatar_url, bio, following, followers });
-  } catch {
-    console.error('error');
-  }
-};
+// export const getProfileInfo = async (setUserInfo: SetterOrUpdater<ProfileType>) => {
+//   try {
+//     const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user`, {
+//       headers: {
+//         Accept: 'application/vnd.github+json',
+//         Authorization: `Bearer ${process.env.NEXT_PUBLIC_GIT_TOKEN}`,
+//       },
+//     });
+//     const { login, avatar_url, bio, following, followers } = response.data;
+//     setUserInfo({ login, avatar_url, bio, following, followers });
+//   } catch {
+//     console.error('error');
+//   }
+// };
+
+export const getProfileInfo = selector({
+  key: 'getProfileInfo',
+  get: async () => {
+    try {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user`, {
+        headers: {
+          Accept: 'application/vnd.github+json',
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_GIT_TOKEN}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('무슨 에러?:', error);
+      throw error;
+    }
+  },
+});
+
+// (setUserInfo: SetterOrUpdater<ProfileType>) => {
+//   try {
+//     const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user`, {
+//       headers: {
+//         Accept: 'application/vnd.github+json',
+//         Authorization: `Bearer ${process.env.NEXT_PUBLIC_GIT_TOKEN}`,
+//       },
+//     });
+//     const { login, avatar_url, bio, following, followers } = response.data;
+//     setUserInfo({ login, avatar_url, bio, following, followers });
+//   } catch {
+//     console.error('error');
+//   }
+// };
