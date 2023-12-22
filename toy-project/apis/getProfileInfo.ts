16b71e3b61bf43858eda64f5d685946b0d9a9ab9
Doyun Lee <doyn511@ajou.ac.kr>
2023-12-22 11:55:06 +0900

    [ etc ] TOKEN 삭제

--- toy-project/apis/getProfileInfo.ts
+++ toy-project/apis/getProfileInfo.ts
@@ -2,14 +2,12 @@ import { ProfileType } from '@/types/types';
 import axios from 'axios';
 import { SetterOrUpdater } from 'recoil';
 
-const GIT_TOKEN = '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';
-
 export const getProfileInfo = async (setUserInfo: SetterOrUpdater<ProfileType>) => {
   try {
     const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user`, {
       headers: {
         Accept: 'application/vnd.github+json',
-        Authorization: `Bearer ${GIT_TOKEN}`,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_GIT_TOKEN}`,
       },
     });
     const { login, avatar_url, bio, following, followers } = response.data;
