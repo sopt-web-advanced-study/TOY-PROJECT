92e18a24166cc6f804ba1da6adf324ee83bb8ab8
Doyun Lee <doyn511@ajou.ac.kr>
2023-12-23 05:32:09 +0900

    [ etc ] 주석 삭제

--- toy-project/apis/getProfileInfo.ts
+++ toy-project/apis/getProfileInfo.ts
@@ -1,6 +1,5 @@
 import { ProfileType } from '@/types/types';
 import axios from 'axios';
-// import { selector } from 'recoil';
 import { SetterOrUpdater } from 'recoil';
 
 export const getProfileInfo = async (setUserInfo: SetterOrUpdater<ProfileType>) => {
@@ -17,21 +16,3 @@ export const getProfileInfo = async (setUserInfo: SetterOrUpdater<ProfileType>) 
     console.error('error');
   }
 };
-
-// export const getProfileInfo = selector({
-//   key: 'getProfileInfo',
-//   get: async () => {
-//     try {
-//       const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/user`, {
-//         headers: {
-//           Accept: 'application/vnd.github+json',
-//           Authorization: `Bearer ${process.env.NEXT_PUBLIC_GIT_TOKEN}`,
-//         },
-//       });
-//       return response.data;
-//     } catch (error) {
-//       console.error('무슨 에러?:', error);
-//       throw error;
-//     }
-//   },
-// });
