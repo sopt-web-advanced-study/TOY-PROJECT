11e30494ce9701a0ed89c8e647ddd47ea7f21054
imeureka <lky062@naver.com>
2023-12-26 00:34:29 +0900

    [ feat ] oauth 상수 변경

---
+++ toy-project/core/oauth.ts
@@ -0,0 +1,7 @@
+export const CLIENT_ID = '5f84d5b2b354a563d8dc';
+export const CLIENT_SECRETS = 'ff81919707c92c516d39a6642205261ff553bbdf';
+export const CALLBACK_URL = 'http://localhost:3000/checkfollow';
+
+export const GITHUB_AUTH_CODE_SERVER = 'https://github.com/login/oauth/authorize';
+export const GITHUB_AUTH_TOKEN_SERVER = '/login/oauth/access_token';
+export const GITHUB_API_SERVER = '/user';
