18a1fa01ea2e93f7794212990756dc9d16418a05
HAE2NI <hae0_02ni@ewhain.net>
2023-12-26 11:38:36 +0900

    [ feat ] 하나만 눌렀을 때에도 체크 표시 나오게 수정

--- toy-project/components/checkfollow/ListElement.tsx
+++ toy-project/components/checkfollow/ListElement.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { checkState } from '@/recoil/atoms';
+import { checkState, loginClick } from '@/recoil/atoms';
 import { followList } from '@/types/types';
 import Image from 'next/image';
 import { useRecoilValue } from 'recoil';
 
 export default function ListElement(props: followList) {
   const selectState = useRecoilValue(checkState);
-  const { id, login, avatar_url } = props;
+  const loginClickState = useRecoilValue(loginClick);
+
+  const { id, login, avatar_url, onClick } = props;
 
   return (
     <li className="flex h-[40px] w-full items-center gap-[10px] px-[44px]">
@@ -16,8 +18,15 @@ export default function ListElement(props: followList) {
       ) : (
         <div className="h-[15px] w-[15px]"></div>
       )}
+      {loginClickState === login ? (
+        <i className="h-[15px] w-[15px] text-subTitle01">✔️</i>
+      ) : (
+        <div className="h-[15px] w-[15px]"></div>
+      )}
       <Image src={avatar_url} alt="팔로우리스트-프로필이미지" width={40} height={40} className="rounded-full" />
-      <span className="text-title01">{login}</span>
+      <span onClick={onClick} className="text-title01">
+        {login}
+      </span>
     </li>
   );
 }
